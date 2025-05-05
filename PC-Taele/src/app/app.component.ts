64f024136788e1aca597fd464301357af23c4860
Ebenezer2077 <tobigcb547@outlook.de>
2025-05-05 22:29:52 +0200

    first step towards saving things on local storage

--- PC-Taele/src/app/app.component.ts
+++ PC-Taele/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StorageService } from './storage.service';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,14 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'PC-Taele';
+  constructor(private storageService: StorageService) {
+  }
+
+  save_string(text: string) {
+    this.storageService.save_string_on_locale_storage(text)
+  }
+
+  load_string() {
+    this.storageService.get_string_from_locale_storage()
+  }
 }
