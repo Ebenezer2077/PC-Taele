cd9656d11a7d9af9b24c39f23c2af9bb6c5b886c
Ebenezer2077 <tobigcb547@outlook.de>
2025-08-14 00:31:56 +0200

    migarted to 19

--- PC-Taele/src/app/app.component.ts
+++ PC-Taele/src/app/app.component.ts
@@ -4,11 +4,10 @@ import { StorageService } from './storage.service';
 import { MainTableComponent } from './main-table/main-table.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, MainTableComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, MainTableComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'PC-Taele';
--- PC-Taele/src/app/main-table/main-table.component.ts
+++ PC-Taele/src/app/main-table/main-table.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-main-table',
-  standalone: true,
-  imports: [],
-  templateUrl: './main-table.component.html',
-  styleUrl: './main-table.component.css'
+    selector: 'app-main-table',
+    imports: [],
+    templateUrl: './main-table.component.html',
+    styleUrl: './main-table.component.css'
 })
 export class MainTableComponent {
 
