b5e1ff3fddbd59dac52d9aed3b7e9022c2298a2e
Ebenezer2077 <tobigcb547@outlook.de>
2025-08-14 00:02:20 +0200

    Added new Component

--- PC-Taele/src/app/app.component.ts
+++ PC-Taele/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StorageService } from './storage.service';
+import { MainTableComponent } from './main-table/main-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
